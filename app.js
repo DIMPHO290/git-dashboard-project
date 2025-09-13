18d105815e59185e9538f2f72a8ecc0a0e49e795
dimpho290 <dimphotshid1@gmail.com>
2025-09-13 23:36:11 +0200

    Added an if statement and fetch error

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const loadBtn = document.getElementById('load-data-btn');
 async function fetchData() {
   try{
      const response = await fetch(API_ENDPOINT);
+     if(!response.ok){
+      throw new Error(`HTTP error! status: ${response.status}`)
+     }
      const data  = await response.json();
      dataContainer.innerHTML = `<strong>Data Loaded</strong> ${data.message}`;
   } catch(error){
-   
+     dataContainer.innerHTML = `<span class = "error">Failed to Load data</span> `;
+    console.error('Fetch Error:', error);
   }
 }
 
